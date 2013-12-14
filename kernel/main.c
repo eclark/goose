16e82e228830a0815dd64b8220cc54677004909a
Eric Clark <eclark@signedint.com>
2013-12-13 18:16:56 -0600

    Simple puts function.

--- kernel/main.c
+++ kernel/main.c
@@ -1,13 +1,35 @@
 
+unsigned short cx, cy;
 unsigned short *vga = (unsigned short*)0xb8000;
+
+void
+puts(const char *str)
+{
+	unsigned short c = 0x0700;
+
+	while (*str != 0) {
+		if (c == '\n') {
+			cy++;
+			cx = 0;
+		} else {
+			c = (c & 0xff00) | *str;
+
+			vga[cy*80 + cx] = c;
+
+			if (++cx == 80)
+				cy++;
+		}
+		cx %= 80;
+		cy %= 25;
+
+		str++;
+	}
+}
 
 void
 main(void)
 {
 	int i;
 
-	for (i = 0; i < 80*25; i++) {
-		vga[i] = 0x0700 + ('a' + i % 26);
-	}
-
+	puts("Simple print");
 }
