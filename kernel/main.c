eae6f9391cf65a85d98c0ce8f5baa3f4f7c698de
Eric Clark <eclark@signedint.com>
2013-12-14 01:48:49 -0600

    Pass multiboot addresses to main()

--- kernel/main.c
+++ kernel/main.c
@@ -3,23 +3,38 @@
 #include <stdint.h>
 
 #include "x86_64.h"
+#include "multiboot2.h"
 #include "vc.h"
 #include "mem.h"
 #include "interrupt.h"
 
 void
-main(void)
+main(uint32_t magic, uint32_t addr)
 {
 	unsigned long long i;
 	clear();
 
+	/*
 	while (1) {
 		puts(" => ");
 		puthex(i, 2);
 		puts(" <=\n");
 
 		i++;
-	}
+	}*/
+
+	puthex(magic, 4);
+	puts("\n");
+	puthex(addr, 4);
+	puts("\n");
+
+
+	struct multiboot_tag* tag = (void*)(HIGH_HALF + addr + 8);
+
+	puts("\nType: ");
+	puthex(tag->type, 4);
+	puts("\nSize: ");
+	puthex(tag->size, 4);
 }
 
 void
@@ -30,6 +45,8 @@ interrupt(regs_t* regs)
 	puthex(regs->vector, 1);
 	puts("\nException: ");
 	puthex(regs->error_code, 8);
+	puts("\nAt: ");
+	puthex(regs->rip, 8);
 	puts("\n\nHalted\n");
 
 	asm volatile("1: hlt; jmp 1b;");
--- kernel/x86_64.h
+++ kernel/x86_64.h
@@ -3,8 +3,8 @@
 
 #define KERNEL_CS 0x08
 #define KERNEL_DS 0x10
-
 #define KERNEL_ESP 0x200000
+#define HIGH_HALF 0xffffffff80000000
 
 #ifndef ASM_FILE
 
