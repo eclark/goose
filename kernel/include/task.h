c1c59eac7ce9a118646ab56a8380ae0fabdc103f
Eric Clark <eclark@signedint.com>
2014-01-12 20:49:15 -0600

    New memory map idea doc

---
+++ kernel/include/task.h
@@ -0,0 +1,10 @@
+#ifndef _TASK_H
+#define _TASK_H
+
+#include <x86_64.h>
+
+typedef struct __attribute__((aligned(8192)) {
+
+} task_t;
+
+#endif
--- kernel/include/x86_64.h
+++ kernel/include/x86_64.h
@@ -3,7 +3,7 @@
 
 #define KERNEL_CS 0x08
 #define KERNEL_DS 0x10
-#define KERNEL_ESP 0x200000
+#define KERNEL_ESP 0x200000 /* Bad news */
 #define HIGH_HALF 0xffff800000000000
 
 /* Initial kernel heap to use before the memory subsystem can assign
